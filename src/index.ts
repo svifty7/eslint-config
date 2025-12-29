ef90d4738dd6b34a79feb6297fb00a89a7dfc3c4
svifty7 <svifty7@gmail.com>
2025-12-30 02:50:04 +0300

    fix: build

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { configure } from './factory';
-
 export * from './configs';
+export { configure } from './factory';
 export * from './factory';
 export * from './globs';
 export * from './types';
 export * from './utils';
-
-export default configure;
